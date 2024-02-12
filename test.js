5f5b9e633e8752bdff4a877d38c2a43bad16ff7f
Vadym <7788853653x@gmail.com>
2024-02-13 00:51:13 +0200

    first commit

---
+++ test.js
@@ -0,0 +1,83 @@
+// первая задача
+
+const myCountry = 'Ukraine';
+console.log(myCountry);
+
+// вторая задача
+
+let isStudent
+isStudent = true;
+console.log(isStudent);
+
+isStudent = folse;
+console.log(isStudent);
+
+// 3 задача
+
+let x = 5;
+let y = 6;
+let result = x * y;
+console.log(result);
+
+// задача 4
+
+let name = "Vadym";
+let lastName = "Lavrenko";
+let profession = "QaEngineer";
+
+
+// задача 5
+
+let p = 5;
+
+// задача 6
+
+let m = "name";
+let n = 5;
+let v = true;
+let w = null;
+let b = undefined;
+
+ 
+ let house = {
+     age: 30,
+    name: "Big house",
+   address: {
+        city: "Kyiv",
+        street: "Freedom",
+        }
+        };
+
+ console.log(house);
+
+ house["Ground floor"] = true;
+
+ console.log(house);
+
+ delete house.age;
+
+ console.log(house);
+
+ let home = object.assign({}, house);
+
+ home.age = 18;
+
+ console.log(house);
+ console.log(home);
+
+
+   {
+        "age": 30,
+    "name": "Big house",
+   "address": {
+        city: "Kyiv",
+        street: "Freedom",
+        }
+       
+    }
+
+    
+ 
+
+
+
