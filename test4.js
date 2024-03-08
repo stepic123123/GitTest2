955178b8d1e758294e4ee86cf16eaeb32ad13cd4
Vadym <7788853653x@gmail.com>
2024-03-08 18:11:03 +0200

    add file

--- test4.js
+++ test4.js
@@ -28,8 +28,10 @@
 
 //При помощи цикла for выведите чётные числа от 2 до 10.
 
-// for (let i = 2; i <= 10; i = i + 2) console.log (i);
-
+// for (let i = 2; i <= 10; i++) {
+//     if (i%2==0){
+//     console.log (i)};
+// };
 // for (let i = 0; i < 3; i++) {
 //     console.log( `number ${i}!` );
 //   }
@@ -42,37 +44,98 @@
 
 // функции
 
-let a = 10;
-let b = 5;
+// let a = 1;
+// let b = 2;
+// let c = a + b;
+
+// console.log(c);
+
+// function sum (a,b) {
+//     const sum = a + b;
+//     console.log(sum);
+// }
+
+// sum(a, b);
+
+// function min (a,b) {
+//     const min = a - b;
+//     console.log (min);
+// }
+
+// min(a, b);
+
+// function mult (a,b) {
+//     const mult = a * b;
+//     console.log (mult);
+// }
+
+// mult (a,b);
+
+// function div (a,b) {
+//     const div = a / b;
+//     console.log (div);
+// }
+
+// div (a,b);
+
+// function summury (a,b) {
+//    const sum2 =  sum(a, b);
+//    const min2 =  min(sum2, b); 
+//    const mult2 = mult (min2,b);
+//    const div2 =  div (mult2,b);
+//    return div2
+// };
+
+// summury (a,b);
+
+
+
+
+
+let a = 1;
+let b = 2;
 let c = a + b;
 
-console.log(c);
+//console.loge.log(c);
 
-function sum (a,b) {
+function sum(a, b) {
     const sum = a + b;
-    console.log(sum);
+    return sum;
 }
 
-sum(a, b);
+//sum(a, b);
 
-function min (a,b) {
+function min(a, b) {
     const min = a - b;
-    console.log (min);
+    return min;
 }
 
-min(a, b);
+//min(a, b);
 
-function mult (a,b) {
+function mult(a, b) {
     const mult = a * b;
-    console.log (mult);
+    return mult;
 }
 
-mult (a,b);
+//mult(a, b);
 
-function div (a,b) {
+function div(a, b) {
     const div = a / b;
-    console.log (div);
+    return div;
 }
 
-div (a,b);
+//div(a, b);
 
+function summury(a, b) {
+    const sum2 = sum(a, b);
+    const min2 = min(sum2, b);
+    const mult2 = mult(min2, b);
+    const div2 = div(mult2, b);
+    console.log(sum2 + ' ' + min2 + ' ' + mult2 + ' ' + div2);
+    return div2;
+}
+
+console.log(summury(a, b));
+
+const sum100 = sum(100, 100);
+console.log(sum100);
